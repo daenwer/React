0912757ae6c69903eba49e23f76c8fdc9896c176
Lazovsky <anton@fksis.by>
2019-08-21 12:22:00 +0300

    added buttons

--- lesson1/classwork/src/App.js
+++ lesson1/classwork/src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
 
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            name: 'React'
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <div className='Inline'>
+                    <Button text='success'/>
+                    <Button text='info'/>
+                    <Button text='warning'/>
+                    <Button text='danger'/>
+                    <Button text='default'/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
