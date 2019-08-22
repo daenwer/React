692157582893dd666630cd03bb518e0d7b9f4191
dmitriy <daenwer@gmail.com>
2019-08-22 19:08:43 +0300

    lesson 2 start

--- lesson2/classwork/src/App.js
+++ lesson2/classwork/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            name: 'React',
+            buttons: ['success', 'info', 'warning', 'danger', 'default'],
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <div className='Inline'>
+                    {/*<Button text='success'/>*/}
+                    {/*<Button text='info'/>*/}
+                    {/*<Button text='warning'/>*/}
+                    {/*<Button text='danger'/>*/}
+                    {/*<Button text='default'/>*/}
+                    {this.state.buttons.map(button => (
+                        <Button text={button} />
+                    ))}
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
