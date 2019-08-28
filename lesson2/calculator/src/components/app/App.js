a187b13acdb58e6739e7ba029f2cb462aa376a67
Lazovsky <anton@fksis.by>
2019-08-28 18:39:44 +0300

    sdelal kal'kulyator 18/5000 made a calculator

--- lesson2/calculator/src/components/app/App.js
+++ lesson2/calculator/src/components/app/App.js
@@ -27,14 +27,14 @@ class App extends Component {
             BUTTONS: {
                 '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
                 '6': 6, '7': 7, '8': 8, '9': 9, '10': '.',
-                '11': '/', '12': '*', '13': '-', '15': '+', '23': '%',
+                '11': '/', '12': '*', '13': '-', '15': '+', '23': '%', '22': '√',
             },
 
             memory: [0],
             memoryDisplay: false,
             number0: [],
             number1: [],
-            act: '',
+            act: null,
             input0: null,
             input1: 0,
         };
@@ -42,6 +42,9 @@ class App extends Component {
         this.changeItem = this.changeItem.bind(this);
         this.changeNumber = this.changeNumber.bind(this);
         this.chooseAction = this.chooseAction.bind(this);
+        this.calculation = this.calculation.bind(this);
+        this.calculationSqrt = this.calculationSqrt.bind(this);
+        this.changeMemory = this.changeMemory.bind(this);
     }
 
     changeNumber(id) {
@@ -51,26 +54,143 @@ class App extends Component {
                 if (id === 10) {
                     if (arr.includes(this.state.BUTTONS[id])) {
                         return;
-                }}
-            arr.push(this.state.BUTTONS[id]);
-            this.setState({number1: arr, input1: arr.join('')})
-        }}
+                    }
+                }
+                if (arr[0] === 0) {arr = [];}
+                arr.push(this.state.BUTTONS[id]);
+                if (this.state.memoryDisplay) {this.memoryDisplay();}
+                this.setState({number1: arr, input1: arr.join('')})
+            }
+        }
     }
 
     chooseAction(id) {
-        console.log(id);
         let arr = [...this.state.number1];
+        if (this.state.memoryDisplay) {this.memoryDisplay();}
         this.setState({number0: arr, act: this.state.BUTTONS[id], number1: [], input0: arr, input1: 0})
     }
 
-    changeItem(id) {
+    changeMemory(act) {
+        if (act === '+') {
+            let result = eval(this.state.memory.join('') + '+' + this.state.number1.join(''));
+            this.setState({memory: String(result).split('')})
+        }
+        if (act === '-') {
+            let result = eval(this.state.memory.join('') + '-' + this.state.number1.join(''));
+            this.setState({memory: String(result).split('')})
+        }
+        if (act === 'delete') {
+            this.setState({memory: [0]})
+        }
+    }
+
+    async calculation() {
+        if (this.state.act === "%") {
+            let result = eval(this.state.number0.join('') + '/100*' + this.state.number1.join(''));
+            if (this.state.memoryDisplay) {this.memoryDisplay();}
+            const badly = await this.setState({
+                number0: [], number1: String(result).split(''),
+                act: null, input0: null, input1: String(result).split('')
+            });
+            return;
+        }
+        if (!this.state.act) {
+            return;
+        }
+        if (this.state.act === '/' && !this.state.number1) {
+            return;
+        }
+        let result = eval(this.state.number0.join('') + this.state.act + this.state.number1.join(''));
+        if (this.state.memoryDisplay) {this.memoryDisplay();}
+        const badly = await this.setState({
+            number0: [], number1: String(result).split(''),
+            act: null, input0: null, input1: String(result).split('')
+        })
+    }
+
+    async calculationSqrt() {
+        const badly = await this.calculation();
+
+        if (this.state.number1.join('') < 0) {
+            return;
+        }
+        let result = Math.sqrt(this.state.number1.join(''));
+        if (this.state.memoryDisplay) {this.memoryDisplay();}
+        this.setState({
+            number0: [], number1: String(result).split(''),
+            act: null, input0: null, input1: String(result).split('')
+        })
+    }
+
+    async changeItem(id) {
+
+        if (id !== 24) {
+            const badly = await this.setState({memoryDisplay: false})
+        }
         if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10].includes(id)) {
             this.changeNumber(id);
         }
         if ([11, 12, 13, 15, 23].includes(id)) {
-            console.log('id = ', id);
+            if (this.state.act) {this.calculation()}
+            if (id === 23 && this.state.number1.length === 0) {return;}
             this.chooseAction(id);
         }
+        if (id === 14) {
+            this.calculation()
+        }
+        if (id === 22) {
+            this.calculationSqrt();
+        }
+        if (id === 20) {
+            if (this.state.memoryDisplay) {this.memoryDisplay();}
+            this.setState({
+                memoryDisplay: false, number0: [], number1: [],
+                act: null, input0: null, input1: 0,
+            })
+        }
+        if (id === 21) {
+            if (this.state.number1.length > 1) {
+                let arr = [...this.state.number1];
+                arr.pop();
+                if (this.state.memoryDisplay) {this.memoryDisplay();}
+                this.setState({number1: arr, input1: arr})
+            } else if (this.state.number1.length === 1) {
+                if (this.state.memoryDisplay) {this.memoryDisplay();}
+                this.setState({number1: [], input1: 0})
+            } else if (!this.state.number1 || this.state.act) {
+                if (this.state.memoryDisplay) {this.memoryDisplay();}
+                this.setState({
+                    number1: this.state.number0,
+                    input1: this.state.number0,
+                    number0: [], act: null, input0: null
+                })
+            }
+        }
+
+        if (id === 24) {
+            this.setState(
+                this.setState({
+                    memoryDisplay: true, number0: [], number1: this.state.memory,
+                    act: null, input0: null, input1: this.state.memory,
+                })
+            )
+        }
+
+        if (id === 26) {
+            this.calculation();
+            this.changeMemory('+');
+        }
+
+        if (id === 25) {
+            this.calculation();
+            this.changeMemory('-');
+        }
+
+        if (id === 27) {
+            this.calculation();
+            this.changeMemory('delete');
+        }
+
     }
 
     arr = [0, 4, 8, 12, 16, 20];
@@ -84,7 +204,7 @@ class App extends Component {
                        memory={this.state.memoryDisplay}/>
                 {this.arr.map((number, i) => (
                     <Button key={number}
-                            arr={this.state.buttons.slice(number, number+4)}
+                            arr={this.state.buttons.slice(number, number + 4)}
                             changeItem={this.changeItem}/>
                 ))}
             </div>
